6de43a80ee05f7079b96f2f4956ac5b51df57eed
abhinand v k <abhinandazokdio@gmail.com>
2022-11-29 17:30:40 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react'
 import CodeMirror from '@uiw/react-codemirror'
 import { javascript } from '@codemirror/lang-javascript'
 import { okaidia } from '@uiw/codemirror-theme-okaidia';
+import useLocalStorage from './hooks/useLocalSotrage';
 function App() {
-  const [datavalue, setValue] = React.useState("console.log('hello world!');")
+  const [value, setValue] = useLocalStorage("console.log('hello world!');")
 
   //change value function
-  const onChange = React.useCallback((value, viewUpdate) => {
-    setValue(value)
+  const onChange = React.useCallback((values, viewUpdate) => {
+    setValue(values)
   }, []);
   return (
     <CodeMirror
-      value={datavalue}
+      value={value}
       height='100vh'
       theme={okaidia}
       extensions={[javascript({ jsx: true })]}
